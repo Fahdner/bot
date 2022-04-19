980e44cbac11a2b35e9d491b70f3027fc802897d
Fahdner <104000222+Fahdner@users.noreply.github.com>
2022-04-19 13:11:39 +0200

    Add files via upload

--- start.php
+++ start.php
@@ -9,7 +9,7 @@ exec('kill -9 ' . file_get_contents($screen . 'pid'));
 file_put_contents($screen . 'pid', getmypid());
 include 'index.php';
 $accounts = json_decode(file_get_contents('accounts.json') , 1);
-$cookies = $accounts[$screen]['cookies'];
+$cookies = $accounts[$screen]['cookies'] . $accounts[$screen]['sessionid'];
 $useragent = $accounts[$screen]['useragent'];
 $users = explode("\n", file_get_contents($screen));
 $uu = explode(':', $screen) [0];
@@ -21,28 +21,27 @@ $yahoo = 0;
 $mailru = 0;
 $true = 0;
 $false = 0;
-$NotBusiness = 0;
 $edit = bot('sendMessage',[
     'chat_id'=>$id,
-    'text'=>"*- ج͜ــ๋͜ـآري͜ــ๋͜ـ آل͜ــ๋͜ـف͒͜ــ๋͜ـح͜ــ๋͜ـص͜ــ๋͜ـ 😈🔥*",
+    'text'=>"- *  جاري الفحص  ✅
+    يمكنك ترك البوت الان حيثما ينتهي 🔥 *",
     'parse_mode'=>'markdown',
     'reply_markup'=>json_encode([
             'inline_keyboard'=>[
-                [['text'=>'عدد الفحص 🎰 :'.$i,'callback_data'=>'fgf']],
-                [['text'=>'عدد اليوزرات 🤖 :'.$user,'callback_data'=>'fgdfg']],
-                [['text'=>"𝓰𝓶𝓪𝓲𝓵 :$gmail",'callback_data'=>'dfgfd'],['text'=>"𝔂𝓪𝓱𝓸𝓸 :$yahoo",'callback_data'=>'gdfgfd']],
-                [['text'=>'𝓶𝓪𝓲𝓵𝓻𝓾 :'.$mailru,'callback_data'=>'fgd'],['text'=>'𝓱𝓸𝓽𝓶𝓪𝓲𝓵 :'.$hotmail,'callback_data'=>'ghj']],
-                [['text'=>'✅- متاح : '.$true,'callback_data'=>'gj']],
-                [['text'=>'❎- ليس متاح : '.$false,'callback_data'=>'dghkf']],
-                [['text'=>'NOT BUSINESS ☑ :'.$NotBusiness,'callback_data'=>'dgdge']],
+                [['text'=>'يوزرات المفحوصة: '.$i,'callback_data'=>'fgf']],
+                [['text'=>'♩على هذا اليوزر: '.$user,'callback_data'=>'fgdfg']],
+                [['text'=>"جيميل: $gmail",'callback_data'=>'dfgfd'],['text'=>"ياهو: $yahoo",'callback_data'=>'gdfgfd']],
+                [['text'=>'روسي: '.$mailru,'callback_data'=>'fgd'],['text'=>'هوتميل: '.$hotmail,'callback_data'=>'ghj']],
+                [['text'=>'تم صيد متاح🔥: '.$true,'callback_data'=>'gj']],
+                [['text'=>'💫ليس متاح: '.$false,'callback_data'=>'dghkf']]
             ]
         ])
 ]);
-$se = 1;
+$se = 100;
 $editAfter = 1;
 foreach ($users as $user) {
     $info = getInfo($user, $cookies, $useragent);
-    if ($info != false and !is_string($info)) {
+    if ($info != false ) {
         $mail = trim($info['mail']);
         $usern = $info['user'];
         $e = explode('@', $mail);
@@ -65,18 +64,7 @@ foreach ($users as $user) {
                                 $follow = $info['f'];
                                 $following = $info['ff'];
                                 $media = $info['m'];
-                                bot('sendMessage', ['disable_web_page_preview' => true, 'chat_id' => $id, 'text' => "➭ 𝐇𝐈 𝐍𝐄𝐖 𝐅𝐔𝐂𝐊𝐄𝐃 𝐀𝐂𝐂𝐎𝐔𝐍𝐓 🎭 
--------------------------------
-𖡦-› 𝐮𝐬𝐞𝐫𝐧𝐚𝐦𝐞 : [$usern](instagram.com/$usern)
-𖡦-› 𝐞𝐦𝐚𝐢𝐥 : [$mail]
-𖡦-› 𝐩𝐡𝐨𝐧𝐞 𝐧𝐮𝐦𝐛𝐞𝐫 : [$phone]
-𖡦-› 𝐟𝐨𝐥𝐥𝐨𝐰𝐞𝐫𝐬 : $follow
-𖡦-› 𝐟𝐨𝐥𝐥𝐨𝐰𝐢𝐧𝐠 : $following
-𖡦-› 𝐩𝐨𝐬𝐭 : $media
-𖡦-› 𝐭𝐢𝐦𝐞 : " . date('Y/n/j g:i') . "
-𖡦-› 𝐚𝐜𝐜𝐨𝐮𝐧𝐭 𝐯𝐞𝐫𝐢𝐟𝐢𝐞𝐝  : false
--------------------------------
-𝐓𝐄𝐋𝐄:  [@Fuck_a1]   〰️   𝐂𝐇: [@dev_a0]",
+                                bot('sendMessage', ['disable_web_page_preview' => true, 'chat_id' => $id, 'text' => "- MUNTAZIR- 👨🏻‍💻\n━━━━━━━━━━━━\n.✥. 𝗨𝘀𝗲𝗿 : [$usern](instagram.com/$usern)\n.✥. 𝗘𝗠𝗔𝗜𝗟 : [$mail]\n.✥. 𝗙𝗼𝗹𝗹𝗼𝘄𝗲𝗿𝘀 : $follow\n.✥. 𝗙𝗼𝗹𝗹𝗼𝘄𝗶𝗻𝗴 : $following\n.✥. 𝗣𝗼𝘀𝘁𝘀 : $media\n━━━━━━━━━━━━\n𝗗𝗲𝘃 :- [@MU_NTW ☕︎︎]\n𝐂𝐇 𝐃𝐄𝐕 :- [@FN_OFO ☕︎︎]",
                                 
                                 'parse_mode'=>'markdown']);
                                 
@@ -85,13 +73,12 @@ foreach ($users as $user) {
                                     'message_id'=>$edit->result->message_id,
                                     'reply_markup'=>json_encode([
                                         'inline_keyboard'=>[
-                                            [['text'=>'عدد الفحص 🎰 :'.$i,'callback_data'=>'fgf']],
-                                            [['text'=>'عدد اليوزرات 🤖 :'.$user,'callback_data'=>'fgdfg']],
-                                            [['text'=>"𝓰𝓶𝓪𝓲𝓵 :$gmail",'callback_data'=>'dfgfd'],['text'=>"𝔂𝓪𝓱𝓸𝓸 :$yahoo",'callback_data'=>'gdfgfd']],
-                                            [['text'=>'𝓶𝓪𝓲𝓵𝓻𝓾 :'.$mailru,'callback_data'=>'fgd'],['text'=>'𝓱𝓸𝓽𝓶𝓪𝓲𝓵 :'.$hotmail,'callback_data'=>'ghj']],
-                                            [['text'=>'✅- متاح : '.$true,'callback_data'=>'gj']],
-                                            [['text'=>'❎- ليس متاح : '.$false,'callback_data'=>'dghkf']],
-                                            [['text'=>'NOT BUSINESS ☑ :'.$NotBusiness,'callback_data'=>'dgdge']],
+                                            [['text'=>'يوزرات المفحوصة: '.$i,'callback_data'=>'fgf']],
+                [['text'=>'♩على هذا اليوزر: '.$user,'callback_data'=>'fgdfg']],
+                [['text'=>"جيميل: $gmail",'callback_data'=>'dfgfd'],['text'=>"ياهو: $yahoo",'callback_data'=>'gdfgfd']],
+                [['text'=>'روسي: '.$mailru,'callback_data'=>'fgd'],['text'=>'هوتميل: '.$hotmail,'callback_data'=>'ghj']],
+                [['text'=>'تم صيد متاح🔥: '.$true,'callback_data'=>'gj']],
+                [['text'=>'💫ليس متاح: '.$false,'callback_data'=>'dghkf']]
                                         ]
                                     ])
                                 ]);
@@ -110,45 +97,34 @@ foreach ($users as $user) {
         } else {
           echo "BlackList - $mail\n";
         }
-    } elseif(is_string($info)){
-        bot('sendMessage',[
-            'chat_id'=>$id,
-            'text'=>"الحساب - `$screen`\n تم حظره من *الفحص*.",
-            'reply_markup'=>json_encode([
-                'inline_keyboard'=>[
-                        [['text'=>'نقل اللسته -✅','callback_data'=>'moveList&'.$screen]],
-                        [['text'=>'حذف الحساب -❎','callback_data'=>'del&'.$screen]]
-                    ]    
-            ]),
-            'parse_mode'=>'markdown'
-        ]);
-        exit;
     } else {
-        $NotBusiness+=1;
         echo "Not Bussines - $user\n";
     }
     usleep(750000);
     $i++;
-    file_put_contents($screen, str_replace($user, '', file_get_contents($screen)));
-    file_put_contents($screen, preg_replace("/(^[\r\n]*|[\r\n]+)[\s\t]*[\r\n]+/", "", file_get_contents($screen)));
     if($i == $editAfter){
         bot('editMessageReplyMarkup',[
             'chat_id'=>$id,
             'message_id'=>$edit->result->message_id,
             'reply_markup'=>json_encode([
                 'inline_keyboard'=>[
-                    [['text'=>'عدد الفحص 🎰 :'.$i,'callback_data'=>'fgf']],
-                    [['text'=>'عدد اليوزرات 🤖 :'.$user,'callback_data'=>'fgdfg']],
-                    [['text'=>"𝓰𝓶𝓪𝓲𝓵 :$gmail",'callback_data'=>'dfgfd'],['text'=>"𝔂𝓪𝓱𝓸𝓸 :$yahoo",'callback_data'=>'gdfgfd']],
-                    [['text'=>'𝓶𝓪𝓲𝓵𝓻𝓾 :'.$mailru,'callback_data'=>'fgd'],['text'=>'𝓱𝓸𝓽𝓶𝓪𝓲𝓵 :'.$hotmail,'callback_data'=>'ghj']],
-                    [['text'=>'✅- متاح : '.$true,'callback_data'=>'gj']],
-                    [['text'=>'❎- ليس متاح : '.$false,'callback_data'=>'dghkf']],
-                    [['text'=>'NOT BUSINESS ☑ :'.$NotBusiness,'callback_data'=>'dgdge']],
+                    [['text'=>'يوزرات المفحوصة: '.$i,'callback_data'=>'fgf']],
+                [['text'=>'♩على هذا اليوزر: '.$user,'callback_data'=>'fgdfg']],
+                [['text'=>"جيميل: $gmail",'callback_data'=>'dfgfd'],['text'=>"ياهو: $yahoo",'callback_data'=>'gdfgfd']],
+                [['text'=>'روسي: '.$mailru,'callback_data'=>'fgd'],['text'=>'هوتميل: '.$hotmail,'callback_data'=>'ghj']],
+                [['text'=>'تم صيد متاح🔥: '.$true,'callback_data'=>'gj']],
+                [['text'=>'💫ليس متاح: '.$false,'callback_data'=>'dghkf']]
                 ]
             ])
         ]);
         $editAfter += 1;
     }
 }
-bot('sendMessage', ['chat_id' => $id, 'text' =>"آن͜ــ๋͜ـت͜ــ๋͜ـۿۿہى آل͜ــ๋͜ـف͒͜ــ๋͜ـح͜ــ๋͜ـص͜ــ๋͜ـ :".explode(':',$screen)[0]]);
+bot('sendMessage', ['chat_id' => $id, 'text' =>"انتهى الفحص : ".explode(':',$screen)[0]]);
+
+
+
+
+
+
 
